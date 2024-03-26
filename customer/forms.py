e191cdd4da3c106977d7cab56bd8558f421de4bf
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-03-26 17:37:55 -0300

    Colocando validador de DDD e numero de telefone

--- customer/forms.py
+++ customer/forms.py
@@ -2,15 +2,23 @@ from django import forms
 from .models import Customer
 
 
+
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+#formulario
+    
+
 class CustomerForm(forms.ModelForm):
     first_name = forms.CharField(label="Nome")
     last_name = forms.CharField(label="Sobrenome")
     email = forms.EmailField(label="E-mail")
-    birth_date = forms.DateField(label="Data de Nascimento")
-    area_code = forms.CharField(label="DDD")
-    phone_number = forms.CharField(label="Número de telefone")
+    birth_date = forms.DateField(label="Data de Nascimento", widget=DateInput())
+    area_code = forms.RegexField(label="DDD", regex=r"^\+?1?[0-9]{2}$", error_messages={"invalid": "Número de DDD inválido"})
+    phone_number = forms.RegexField(label="Número de telefone", regex=r"^\+?1?[0-9]{9,15}$", error_messages={"invalid": "Número de Telefone inválido"})
     country = forms.CharField(label="País")
-    state = forms.CharField(label="Estado")
+    state = forms.CharField(label="Estado") 
     city = forms.CharField(label="Cidade")
 
     class Meta:
