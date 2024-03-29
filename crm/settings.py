a659741e5eabe54f173a2c254b9585be4e7545a1
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2024-03-29 19:03:03 -0300

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # }
 
 DATABASES = {}
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}")
 
 
 # Password validation
